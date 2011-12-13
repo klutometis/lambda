7013a09fbb0ddd67634a23434721262dddf742c5
Peter Danenberg <pcd@roxygen.org>
2011-12-13 09:56:35 -0800

    Add the URL to the project.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject lambda "1.0.1-SNAPSHOT"
   :description "fn is ok; but we prefer λ or lambda."
+  :url https://github.com/klutometis/lambda
   :dependencies [[org.clojure/clojure "1.3.0"]])
